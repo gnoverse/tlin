1238551a51d19b7ef58b6a8ba976a93259ea63c7
Lee ByeongJun <lbj199874@gmail.com>
2024-07-24 13:48:47 +0900

    fix formatter

--- internal/lints/cyclomatic_complexity.go
+++ internal/lints/cyclomatic_complexity.go
@@ -2,6 +2,7 @@ package lints
 
 import (
 	"fmt"
+	"go/ast"
 	"go/parser"
 	"go/token"
 
@@ -19,13 +20,27 @@ func DetectHighCyclomaticComplexity(filename string, threshold int) ([]tt.Issue,
 	stats := gocyclo.AnalyzeASTFile(f, fset, nil)
 	var issues []tt.Issue
 
+	funcNodes := make(map[string]*ast.FuncDecl)
+	ast.Inspect(f, func(n ast.Node) bool {
+		if fn, ok := n.(*ast.FuncDecl); ok {
+			funcNodes[fn.Name.Name] = fn
+		}
+		return true
+	})
+
 	for _, stat := range stats {
 		if stat.Complexity > threshold {
+
+			funcNode, ok := funcNodes[stat.FuncName]
+			if !ok {
+				continue
+			}
+
 			issue := tt.Issue{
 				Rule:       "high-cyclomatic-complexity",
 				Filename:   filename,
-				Start:      fset.Position(token.Pos(stat.Pos.Offset)),
-				End:        fset.Position(token.Pos(stat.Pos.Offset)), // End position is not provided by gocyclo
+				Start:      fset.Position(funcNode.Pos()),
+				End:        fset.Position(funcNode.End()),
 				Message:    fmt.Sprintf("function %s has a cyclomatic complexity of %d (threshold %d)", stat.FuncName, stat.Complexity, threshold),
 				Suggestion: "Consider refactoring this function to reduce its complexity. You can split it into smaller functions or simplify the logic.\n",
 				Note:       "High cyclomatic complexity can make the code harder to understand, test, and maintain. Aim for a complexity score of 10 or less for most functions.\n",
