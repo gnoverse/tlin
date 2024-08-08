1dd169a8a33f500f70951f88dae6ee37f8e5abd1
Lee ByeongJun <lbj199874@gmail.com>
2024-08-08 16:40:28 +0900

    fix: remove unnecessary emit specific suggestion (#52)

--- formatter/format_emit.go
+++ formatter/format_emit.go
@@ -1,9 +1,6 @@
 package formatter
 
 import (
-	"fmt"
-	"strings"
-
 	"github.com/gnoswap-labs/lint/internal"
 	tt "github.com/gnoswap-labs/lint/internal/types"
 )
@@ -16,29 +13,7 @@ func (f *EmitFormatFormatter) Format(issue tt.Issue, snippet *internal.SourceCod
 		AddHeader().
 		AddCodeSnippet().
 		AddUnderlineAndMessage().
-		AddEmitFormatSuggestion().
+		AddSuggestion().
 		AddNote().
 		Build()
-}
-
-func (b *IssueFormatterBuilder) AddEmitFormatSuggestion() *IssueFormatterBuilder {
-	if b.issue.Suggestion == "" {
-		return b
-	}
-
-	maxLineNumWidth := calculateMaxLineNumWidth(b.issue.End.Line)
-	padding := strings.Repeat(" ", maxLineNumWidth+1)
-
-	b.result.WriteString(suggestionStyle.Sprint("Suggestion:\n"))
-	b.result.WriteString(lineStyle.Sprintf("%s|\n", padding))
-
-	suggestionLines := strings.Split(b.issue.Suggestion, "\n")
-	for i, line := range suggestionLines {
-		lineNum := fmt.Sprintf("%*d", maxLineNumWidth, b.issue.Start.Line+i)
-		b.result.WriteString(lineStyle.Sprintf("%s | %s\n", lineNum, line))
-	}
-
-	b.result.WriteString(lineStyle.Sprintf("%s|\n", padding))
-
-	return b
 }
