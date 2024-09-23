f07c01b68e315eb608084f8172bd94592bd7a6af
0xtekgrinder <0xtekgrinder@protonmail.com>
2024-09-23 22:56:21 +0200

    feat: type IssueEssential

--- internal/types/types.go
+++ internal/types/types.go
@@ -23,3 +23,14 @@ func (i Issue) String() string {
 		"rule: %s, filename: %s, message: %s, start: %s, end: %s, confidence: %.2f",
 		i.Rule, i.Filename, i.Message, i.Start, i.End, i.Confidence)
 }
+
+// IssueEssential represents a simplified version of Issue meant to be used for JSON output.
+type IssueEssential struct {
+	Rule       string
+	Category   string
+	Message    string
+	Suggestion string
+	Note       string
+	Snippet    string
+	Confidence float64 // 0.0 to 1.0
+}
