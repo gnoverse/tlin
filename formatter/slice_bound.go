535f5b5bc21c520e2d9d6a7634df52a9e19a7112
0xtekgrinder <0xtekgrinder@protonmail.com>
2024-10-24 15:33:37 +0200

    refactor: use text templates for formatting issues

--- formatter/slice_bound.go
+++ formatter/slice_bound.go
@@ -1,32 +1,23 @@
 package formatter
 
-import (
-	"github.com/gnolang/tlin/internal"
-	tt "github.com/gnolang/tlin/internal/types"
-)
-
 type SliceBoundsCheckFormatter struct{}
 
-func (f *SliceBoundsCheckFormatter) Format(
-	issue tt.Issue,
-	snippet *internal.SourceCode,
-) string {
-	builder := newIssueFormatterBuilder(issue, snippet)
-	return builder.
-		AddHeader().
-		AddCodeSnippet().
-		AddUnderlineAndMessage().
-		AddWarning().
-		Build()
+func (f *SliceBoundsCheckFormatter) IssueTemplate() string {
+	return `{{header .Rule .Severity .MaxLineNumWidth .Filename .StartLine .StartColumn}}
+{{snippet .SnippetLines .StartLine .EndLine .MaxLineNumWidth .CommonIndent .Padding}}
+{{underlineAndMessage .Message .Padding .StartLine .EndLine .StartColumn .EndColumn .SnippetLines .CommonIndent}}
+{{warning .Category}}
+`
 }
 
-func (b *issueFormatterBuilder) AddWarning() *issueFormatterBuilder {
-	b.result.WriteString(warningStyle.Sprint("warning: "))
-	if b.issue.Category == "index-access" {
-		b.result.WriteString("Index access without bounds checking can lead to runtime panics.\n")
-	} else if b.issue.Category == "slice-expression" {
-		b.result.WriteString("Slice expressions without proper length checks may cause unexpected behavior.\n\n")
+func warning(category string) string {
+	var endString string
+	endString = warningStyle.Sprint("warning: ")
+	if category == "index-access" {
+		endString += "Index access without bounds checking can lead to runtime panics.\n"
+	} else if category == "slice-expression" {
+		endString += "Slice expressions without proper length checks may cause unexpected behavior.\n"
 	}
 
-	return b
+	return endString
 }
